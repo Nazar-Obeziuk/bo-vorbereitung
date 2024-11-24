1d20269babb258a859a00101fcfa43ba69c043c7
Nazar Obeziuk <nazarobeziuk@MacBook-Pro-Nazar.local>
2024-11-24 14:57:27 +0100

    ui kit

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bo vorbereitung</title>
+    <link rel="stylesheet" href="./styles/main.css" />
+</head>
+
+<body>
+    <!-- <?php include "./components/header.php" ?> -->
+    <div class="container">
+        <button class="primary-button" type="button">
+            ist kostenlos?
+        </button>
+        <button class="primary-button" type="button">
+            <svg width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M7.64648 7.07361C7.81728 6.54732 8.13015 6.07896 8.55078 5.71948C8.97141 5.36001 9.4838 5.12378 10.0303 5.03708C10.5768 4.95038 11.1362 5.0164 11.6475 5.22803C12.1587 5.43966 12.6014 5.78875 12.9268 6.23633C13.2521 6.68391 13.4469 7.21256 13.4904 7.76416C13.5339 8.31576 13.4238 8.86879 13.1727 9.36182C12.9215 9.85484 12.5394 10.2685 12.0676 10.5576C11.5958 10.8467 11.0533 10.9998 10.5 10.9998V12.0002M10.5 19C5.52944 19 1.5 14.9706 1.5 10C1.5 5.02944 5.52944 1 10.5 1C15.4706 1 19.5 5.02944 19.5 10C19.5 14.9706 15.4706 19 10.5 19ZM10.5498 15V15.1L10.4502 15.1002V15H10.5498Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
+            ist kostenlos?
+        </button>
+        <button class="primary-button big" type="button">
+            Log In
+        </button>
+        <button class="primary-button big disabled" type="button">
+            Log In
+        </button>
+        <button class="primary-button border" type="button">
+            <svg width="27" height="16" viewBox="0 0 27 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M1.5 8.00016L8.16667 14.6668M1.5 8.00016L8.16667 1.3335M1.5 8.00016H25.5" stroke="#161616" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
+            Zurück
+        </button>
+        <button class="primary-button border" type="button">
+            <svg width="19" height="12" viewBox="0 0 19 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M16.8337 2.33325L9.50033 9.66659L2.16699 2.33325" stroke="#161616" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
+            Andere Übungen
+        </button>
+        <button class="primary-button border small" type="button">
+            <svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M19.5 21C19.5 17.134 16.366 14 12.5 14C8.63401 14 5.5 17.134 5.5 21M12.5 11C10.2909 11 8.5 9.20914 8.5 7C8.5 4.79086 10.2909 3 12.5 3C14.7091 3 16.5 4.79086 16.5 7C16.5 9.20914 14.7091 11 12.5 11Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
+            Mein account
+        </button>
+        <button class="secondary-button" type="button">
+            Hören Teil 1
+        </button>
+        <button class="secondary-button border" type="button">
+            <svg width="23" height="20" viewBox="0 0 23 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M19.644 9.51118C19.644 5.21563 16.1618 1.7334 11.8662 1.7334C7.57066 1.7334 4.08843 5.21563 4.08843 9.51118M16.3107 13.4001V15.6223C16.3107 16.1386 16.3107 16.3967 16.3534 16.6114C16.5287 17.4929 17.2179 18.1824 18.0994 18.3577C18.3141 18.4004 18.5722 18.4004 19.0884 18.4004C19.6047 18.4004 19.8629 18.4004 20.0775 18.3577C20.9591 18.1824 21.648 17.4929 21.8234 16.6114C21.8661 16.3967 21.8662 16.1386 21.8662 15.6223V13.4001C21.8662 12.8838 21.8661 12.6251 21.8234 12.4105C21.648 11.5289 20.9591 10.8403 20.0775 10.665C19.8629 10.6223 19.6047 10.6223 19.0884 10.6223C18.5722 10.6223 18.3141 10.6223 18.0994 10.665C17.2179 10.8403 16.5287 11.5289 16.3534 12.4105C16.3107 12.6251 16.3107 12.8838 16.3107 13.4001ZM7.42177 13.4001V15.6223C7.42177 16.1386 7.42161 16.3967 7.37891 16.6114C7.20356 17.4929 6.51456 18.1824 5.63303 18.3577C5.41837 18.4004 5.16032 18.4004 4.64404 18.4004C4.12776 18.4004 3.86961 18.4004 3.65495 18.3577C2.77341 18.1824 2.08426 17.4929 1.90891 16.6114C1.86621 16.3967 1.86621 16.1386 1.86621 15.6223V13.4001C1.86621 12.8838 1.86621 12.6251 1.90891 12.4105C2.08426 11.5289 2.77341 10.8403 3.65495 10.665C3.86961 10.6223 4.12775 10.6223 4.64404 10.6223C5.16032 10.6223 5.41837 10.6223 5.63303 10.665C6.51456 10.8403 7.20356 11.5289 7.37891 12.4105C7.42161 12.6251 7.42177 12.8838 7.42177 13.4001Z" stroke="#F7631B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
+            Hören
+        </button>
+        <button class="secondary-button" type="button">
+            Text
+        </button>
+    </div>
+    <!-- <?php include "./components/footer.php" ?> -->
+</body>
+
+</html>
