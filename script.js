21401a716bb20719e63ead12eb9fb83dc9fa1258
Nazar Obeziuk <nazarobeziuk@MacBook-Pro-Nazar.local>
2024-12-02 14:49:31 +0100

    pulling data

--- script.js
+++ script.js
@@ -34,3 +34,21 @@ var swiper = new Swiper(".reviews_swiper", {
     prevEl: arrowReviewsLeft,
   },
 });
+
+// progress bar
+
+const progressSlider = document.getElementById("progress-slider");
+const progressSelectorValue = document.getElementById(
+  "progress-selector-value"
+);
+const progressBar = document.getElementById("progress-bar");
+progressSelectorValue.innerHTML = progressSlider.value + "%";
+
+progressSlider.oninput = function () {
+  progressSelectorValue.innerHTML = this.value + "%";
+  //const left = (((value - minValue) / (valueMax - valueMin)) * ((totalInputWidth - thumbHalfWidth) - thumbHalfWidth)) + thumbHalfWidth;
+  const currentThumbPosition =
+    (this.value / 100) * (this.clientWidth - 22 - 22);
+  progressSelectorValue.style.left = currentThumbPosition + "px";
+  progressBar.style.width = currentThumbPosition + 8 + "px";
+};
