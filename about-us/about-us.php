1e08f508ac2d967d6d1b9909f872151ef6abe1d9
Denys Paziak <67861412+Denys-Paziak@users.noreply.github.com>
2024-12-15 14:17:19 +0100

    about us

---
+++ about-us/about-us.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vorbereitung mit BO - B1 (Goethe)</title>
+    <meta name="description" content="Online Prüfungsvorbereitung für Deutsch Test für Zuwanderer (A1, A2, B1, B2, C1, C2 )">
+    <meta name="keywords" content="Vorbereitung, Deutsch, Kurs, Test">
+    <meta property="og:image" content="https://deutsch-vorbereitung.com/img/logo_medium.png">
+    <meta property="og:title" content="Vorbereitung mit BO">
+    <meta property="og:description" content="Deutsch Prüfung - Vorbereitung auf Hören, Lesen, Schreiben und Sprechen.">
+    <link rel="stylesheet" href="../styles/main.css" />
+    <link rel="stylesheet" href="../styles/about-us/about-us.css" />
+</head>
+
+<body>
+    <?php include dirname(__DIR__) . "/components/header.php"; ?>
+    
+    <section class="about-us">
+        <div class="container">
+            <h1><span>Learn German</span> with us. Guaranteed</h1>
+            <img class="hero-image" src="../images/learn-german-hero.webp" alt="Hero image">
+            <div class="row">
+                <div class="about-us-inner">
+                    <h2><span>15 years </span></h2>
+                    <p class="text p1"><strong>Experience counts</strong></p>
+                    <p class="text p1">since 1952, worldwide presence teaching German.</p>
+                    <p class="text p1 about-us-inner__last">The Goethe-Institut is the Republic of Germany’s official cultural institute. With around 270,000 course participants annually, it is the largest provider of German courses worldwide.</p>
+                </div>
+
+                <div class="about-us-inner">
+                    <h2><span>1120 thousand</span></h2>
+                    <p class="text p1"><strong>Course participants</strong></p>
+                    <p class="text p1">successfully learnt German at a Goethe-Institut in 2019.</p>
+                    <p class="text p1 about-us-inner__last">The Goethe-Institut stands for the competent and efficient teaching of German as a foreign language. Our recipe for success: lessons are based on clear learning goals and focus on your needs.</p>
+                </div>
+            </div>
+        
+    </section>
+
+    <section class="learning-success">
+            <div class="container">
+                <div>
+                    <h2><span>Learning success</span> at the Deutsch  Vorbereitung</h2>
+                    <h3>The Deutsch Vorbereitung is Germany’s best language institute!</h3>
+                    <p class="text p1">That was the result of a representative consumer survey conducted by the ServiceValue market research institute on behalf of the Handelsblatt newspaper. Customers view the Goethe-Institut as by far the best language school provider based on their experiences over the past two years. <br/> <br/>
+                        Source: Handelsblatt, No. 174, 10.09.19, p. 43. <br/> <br/>
+                        A user survey conducted by Statista, an independent market research institute, on behalf of stern magazine to establish the best offers for pupils and adults in continuing education, also came to the same conclusion. Our learning quality was given the best and, at the same time, most important assessment in the survey. Course participants also assigned us with the best price-performance ratio.<br/> <br/>
+                        Source: stern, No. 35, 22.08.19, p. 93.</p>
+                </div>
+                <div>
+                    <h3>he Deutsch Vorbereitung is Germany’s best digital education provider!</h3>
+                    <p class="text p1">That was the result of a representative consumer survey conducted by the ServiceValue market research institute on behalf of the Handelsblatt newspaper. Customers view the Goethe-Institut as by far the best language school provider based on their experiences over the past two years.  <br/> <br/>
+                    Source: Handelsblatt, No. 174, 10.09.19, p. 43.  <br/> <br/>
+                    A user survey conducted by Statista, an independent market research institute, on behalf of stern magazine to establish the best offers for pupils and adults in continuing education, also came to the same conclusion. Our learning quality was given the best and, at the same time, most important assessment in the survey. Course participants also assigned us with the best price-performance ratio.  <br/> <br/>
+                    Source: stern, No. 35, 22.08.19, p. 93.</p>
+                </div>
+            </div>
+    </section>
+
+    <section class="media-library">
+        <div class="container">
+            <h2>Every media library has <span>four zones:</span></h2>
+            <div class="grid">
+                <div class="media-library-box">
+                    <p class="text p1"><span>Learning</span></p>
+                    <p class="text p2">Here you can look for information, prepare for a lesson or an exam without being disturbed.</p>
+                </div>
+                <div class="media-library-box">
+                    <p class="text p1"><span>Communicating</span></p>
+                    <p class="text p2">This area is designed for you to engage in conversation, discuss and work with your fellow students.</p>
+                </div>
+                <div class="media-library-box">
+                    <p class="text p1"><span>Informing</span></p>
+                    <p class="text p2">In this zone, you will find learning materials, information about Germany, books and presentations.</p>
+                </div>
+                <div class="media-library-box">
+                    <p class="text p1"><span>Relaxing</span></p>
+                    <p class="text p2">This is where you can relax and drink some coffee or read a newspaper.</p>
+                </div>
+            </div>
+
+            <div class="line"></div>
+
+            <div class="row">
+                 <img src="../images/media-library.webp" alt="Media library image">
+                <p class="text p1">At the Goethe-Institut, you can also use the free “Deutsch für Dich” digital service for practising and communicating. The community gives you access to extensive, interactive learning materials, games and exercises. Here you can network online with other people learning German and stay in touch with them after the course ends. That’s how we create a stimulating learning environment for learning German. Guaranteed.</p>
+            </div>
+        </div>
+    </section>
+
+    <section class="online-community">
+        <div class="container">
+            <h2><span>Online community</span> “Deutsch für Dich”</h2>
+            <p class="text p1 description">The Goethe-Instituts in Germany offer the entire range of courses – general language courses, exam preparation and job-related courses – for every language level and to meet every individual requirement. You can choose from intensive and evening courses to suit your needs, possibilities and language skills.</p>
+             <div class="row">
+                <div class="online-community-box">
+                    <img src="../images/online-community1.webp" alt="Online community image">
+                    <p class="text p1 online-community-box__title"><strong>Regardless of the level</strong></p>
+                    <p class="text p1">The aim of the lessons is that you enjoy them and make rapid and lasting progress while improving your active language skills. You will train all the language skills – listening, reading, writing and speaking – in the learning and lesson phases, using up-to-date texts, authentic speaking and writing exercises, role plays related to everyday life, and practical project and research exercises.</p>
+                </div>
+
+                <div class="online-community-box">
+                    <img src="../images/online-community2.webp" alt="Online community image">
+                    <p class="text p1 online-community-box__title"><strong>Regardless of the level</strong></p>
+                    <p class="text p1">The aim of the lessons is that you enjoy them and make rapid and lasting progress while improving your active language skills. You will train all the language skills – listening, reading, writing and speaking – in the learning and lesson phases, using up-to-date texts, authentic speaking and writing exercises, role plays related to everyday life, and practical project and research exercises.</p>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+    <section class="guarantee">
+        <div class="container">
+            <h2>The Deutsch Vorbereitung <span>guarantee</span></h2>
+
+            <div class="grid">
+                <div class="guarantee-box">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 60 60" fill="none">
+                        <rect width="60" height="60" rx="30" fill="#F8F8FA"/>
+                        <path d="M14.6641 29.9991L24.1511 39.4861L44.4821 19.157" stroke="#F7631B" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                    <p class="text p2">Individual grading into the appropriate level (online or at the location)</p>
+                </div>
+
+                 <div class="guarantee-box">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 60 60" fill="none">
+                        <rect width="60" height="60" rx="30" fill="#F8F8FA"/>
+                        <path d="M14.6641 29.9991L24.1511 39.4861L44.4821 19.157" stroke="#F7631B" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                    <p class="text p2">Homogeneous learning groups and intensive support makes learning fun and gives you a sense of achievement</p>
+                </div>
+
+                 <div class="guarantee-box">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 60 60" fill="none">
+                        <rect width="60" height="60" rx="30" fill="#F8F8FA"/>
+                        <path d="M14.6641 29.9991L24.1511 39.4861L44.4821 19.157" stroke="#F7631B" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                    <p class="text p2">Friendly and safe atmosphere due to optimal course sizes with max. 16 participants (18 participants in online courses)</p>
+                </div>
+
+                 <div class="guarantee-box">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 60 60" fill="none">
+                        <rect width="60" height="60" rx="30" fill="#F8F8FA"/>
+                        <path d="M14.6641 29.9991L24.1511 39.4861L44.4821 19.157" stroke="#F7631B" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                    <p class="text p2">Communicative exercises and projects will enable you to use the language in a wide variety of everyday, work and study situations</p>
+                </div>
+
+                 <div class="guarantee-box">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 60 60" fill="none">
+                        <rect width="60" height="60" rx="30" fill="#F8F8FA"/>
+                        <path d="M14.6641 29.9991L24.1511 39.4861L44.4821 19.157" stroke="#F7631B" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                    <p class="text p2">Active learning with all the senses: listening, reading, writing and speaking</p>
+                </div>
+
+                 <div class="guarantee-box">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 60 60" fill="none">
+                        <rect width="60" height="60" rx="30" fill="#F8F8FA"/>
+                        <path d="M14.6641 29.9991L24.1511 39.4861L44.4821 19.157" stroke="#F7631B" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                    <p class="text p2">Syllabus in accordance with the Common European Framework of Reference</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <?php include dirname(__DIR__) . "/components/footer.php" ?>
+    <script src="../script.js"></script>
+</body>
